d01c402843eefd9df1d424aeaa697933c8950326
Schüler/in <student@bbk.schule>
2021-11-19 13:32:44 +0100

    aUSKOMMENTIERTEs gelöscht

--- mainform.cpp
+++ mainform.cpp
@@ -7,28 +7,12 @@ MainForm::MainForm( QWidget* parent )
     setupUi(this);
     dice << dice_1 << dice_2 << dice_3 << dice_4 << dice_5;
     lcds << lcdNumber << lcdNumber_2 << lcdNumber_3 << lcdNumber_4 << lcdNumber_5;
-//  QGridLayout* diceLayout = new QGridLayout( this );
-//  QPushButton* btnRollDice = new QPushButton( "&Würfeln" );
-//  diceLayout->addWidget( btnRollDice, 0, 0, 1, DICE_COUNT);
-//  diceLayout->setRowMinimumHeight(2, 50);
-      for( int i = 0; i < dice.count(); i++ ){
-//    DiceWidget* newDice = new DiceWidget;
-//    diceLayout->addWidget( newDice, 1, i, nullptr );
-//    dice[ i ] = newDice;
-      connect( btnRollDice, SIGNAL( clicked() ),
-               dice[i], SLOT( rollDiceSlot() ) );
 
-//    QLCDNumber* newNumber = new QLCDNumber;
-//    newNumber->setDigitCount(1);
-//    //newNumber->setDecMode(); UNNÖTIG
-//    diceLayout->addWidget( newNumber, 2, i);
-//    lcds[i] = newNumber;
-      lcds[i]->display(dice[i]->getValue());
-      connect( dice[i], SIGNAL( valueChanged(int) ),
+    for( int i = 0; i < dice.count(); i++ ){
+        connect( btnRollDice, SIGNAL( clicked() ),
+                 dice[i], SLOT( rollDiceSlot() ) );
+        lcds[i]->display(dice[i]->getValue());
+        connect( dice[i], SIGNAL( valueChanged(int) ),
                  lcds[i], SLOT( display(int) ));
-
     }
-
-
-
 }
